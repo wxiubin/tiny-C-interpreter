2cd57cf2988d17e1350b8f19a1eed775b6f761a2
王修斌 <iosgg@outlook.com>
2019-09-19 22:15:31 +0800

    memset data/text/stack segment

--- xc-tutor.c
+++ xc-tutor.c
@@ -10,6 +10,11 @@ char *src, *old_src;	// pointer to source code string
 int poolsize;			// defaults size of text/data/stack
 int line;				// line number
 
+int *text,				// text segment
+	*old_text,			// for dump text segment
+	*stack;				// stack
+char *data;				// data segment
+
 /// 用于词法分析，获取下一个标记，它将自动忽略空白字符
 void next() {
 	token = *src++;
@@ -60,6 +65,26 @@ int main(int argc, char *argv[]) {
 	
 	src[i] = 0; // add EOF character
 	close(fd);
+	
+	// allocate memory for virtual machine
+	if (!(text = old_text = malloc(poolsize))) {
+		printf("could not malloc(%d) for text area\n", poolsize);
+		return -1;
+	}
+	
+	if (!(data = malloc(poolsize))) {
+		printf("could not malloc(%d) for data area\n", poolsize);
+		return -1;
+	}
+	
+	if (!(stack = malloc(poolsize))) {
+		printf("could not malloc(%d) for stack area\n", poolsize);
+		return -1;
+	}
+	
+	memset(text, 0, poolsize);
+	memset(data, 0, poolsize);
+	memset(stack, 0, poolsize);
 	
 	program();
 	return eval();
